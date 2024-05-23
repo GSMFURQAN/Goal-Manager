1f0a7a8463ef1d73ec99cc26c197683f1a13b797
GSMFURQAN <gsmfurqan@gmail.com>
2024-05-23 12:55:15 +0530

    calendar and timeline

---
+++ client/src/components/ListGoals/AddGoal.js
@@ -0,0 +1,241 @@
+import React, { useEffect, useState } from "react";
+import { Box, Button, Modal, Switch, TextField, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Tooltip from "@mui/material/Tooltip";
+import Stack from "@mui/material/Stack";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import CloseIcon from "@mui/icons-material/Close";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import moment from "moment/moment";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewTodo, getTodos, updateTodo } from "../../Apis";
+import dayjs from "dayjs";
+import {
+  LocalizationProvider,
+  MobileDateTimePicker,
+} from "@mui/x-date-pickers";
+import { fetchData } from "../../redux/goalSlice";
+import { getProgress, selectSnack, selectView } from "../../redux/generalSlice";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+const AddGoal = ({ open, setOpen, id }) => {
+  const general = useSelector((state) => state.general);
+  const { data, loading, error } = useSelector((state) => state.goal);
+  const [parentDate, setParentDate] = useState("");
+  const [goalData, setGoalData] = React.useState({
+    title: "",
+    note: "",
+    dueDate: "",
+    subTasks: [],
+    major:false
+  });
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (general.action == "edit") {
+      fetchIdData();
+    }
+  }, [id, dispatch]);
+
+  const fetchIdData = async () => {
+    await getTodos({ id: id }).then((res) => {
+      setGoalData({
+        ...res.data[0],
+        dueDate: dayjs(res?.data[0]?.dueDate),
+        // major: res.data[0]?.major
+      });
+      setParentDate(res?.data[0]?.dueDate);
+    });
+  };
+  const handleAdd = async () => {
+    const days = moment(
+      id && goalData.dueDate.$d ? goalData.dueDate.$d : goalData.dueDate
+    ).diff(moment(), "days");
+    const goalView =
+    days < 1 ? "day" : days < 32 ? "month" : days < 366 ? "year" : "future";
+    const formData = new FormData();
+    formData.append('title', goalData.title);
+    formData.append('note', goalData.note);
+    formData.append('dueDate', goalData.dueDate);
+    formData.append('done', false);
+    formData.append('dayView', goalView);
+    formData.append('parentIdviewed', '');
+    formData.append('viewed', 'NO');
+    formData.append('major', goalData.major || false);
+    formData.append('image', goalData.image);
+   id && formData.append('_id', id) ;
+    console.log('dswe',goalData.image, formData)
+    // const payload = {
+    //   title: goalData.title,
+    //   note: goalData.note,
+    //   dueDate: goalData.dueDate,
+    //   done: false,
+    //   dayView: goalView,
+    //   parentId: "",
+    //   viewed: "NO",
+    //   major: goalData.major,
+    //   image:goalData.image
+    // };
+    // console.log('ddd',moment( payload.dueDate).diff(parentDate, 'hours') >1)
+    if (
+      general.action == "subTask" &&
+      moment(formData.dueDate).diff(parentDate, "hours") > 1
+    ) {
+      dispatch(
+        selectSnack({
+          severity: "error",
+          message: "selcted date must be less than parent",
+          open: true,
+        })
+      );
+    } else if (general.action == "edit") {
+      await updateTodo( formData).then(async () => {
+        dispatch(fetchData({ dayView: general.dayView }));
+        dispatch(selectView({ ...general, id: "", action: "" }));
+      });
+      setOpen(false);
+    } else if (general.action == "subTask") {
+      await addNewTodo({ ...formData, parentId: id, subTask: true }).then(() => {
+        dispatch(fetchData({ dayView: general.dayView }));
+        dispatch(selectView({ ...general, id: "", action: "" }));
+      });
+      setOpen(false);
+    } else {
+      await addNewTodo(formData).then(() => {
+        dispatch(fetchData({ dayView: general.dayView }));
+      });
+      setOpen(false);
+    }
+    setGoalData({});
+    dispatch(getProgress({ dayView: general.dayView }));
+  };
+
+  useEffect(() => {
+    return () => {};
+  }, [dispatch]);
+  return (
+    <div>
+      {" "}
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+          dispatch(selectView({ ...general, id: "", action: "" }));
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={["DateTimePicker"]}>
+              <DemoItem label={"Add Goal"}>
+                <Stack display={"flex"} direction={"column"} spacing={2}>
+                  <TextField
+                    label="Title"
+                    type="text"
+                    value={goalData.title}
+                    onChange={(e) =>
+                      setGoalData({ ...goalData, title: e.target.value })
+                    }
+                  />
+                  <TextField
+                    multiline
+                    label="note"
+                    type="text"
+                    value={goalData.note}
+                    onChange={(e) =>
+                      setGoalData({ ...goalData, note: e.target.value })
+                    }
+                  />
+
+                  <MobileDateTimePicker
+                    label="Due Date"
+                    sx={{ py  : "12px" }}
+                    value={goalData.dueDate}
+                    onChange={(newValue) =>
+                      setGoalData({ ...goalData, dueDate: newValue.$d })
+                    }
+                  />
+                  <Stack display={'flex'} direction={'row'} justifyContent={'space-between'}>
+
+                  <Button
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    // sx={{ width:'70%', paddingLeft:'12px' }}
+                    onChange={(e)=>setGoalData({...goalData,image:e.target.files[0]})}
+                    startIcon={<CloudUploadIcon />}
+                    >
+                    Upload Image
+                    <VisuallyHiddenInput type="file" />
+                  </Button>
+                  <Box display={'flex'}>
+                    <Typography my={'auto'} >Major Goal</Typography>
+                  <Switch checked={goalData.major} onChange={()=>setGoalData({...goalData,major:!goalData.major})}/>
+                  </Box>
+                    
+                </Stack>
+                </Stack>
+
+                <Stack
+                  pt={2}
+                  display={"flex"}
+                  direction={"row"}
+                  alignSelf={"flex-end"}
+                  spacing={2}
+                  >
+                  <Button
+                    variant="outlined"
+                    onClick={() => {
+                      setOpen(false);
+                      dispatch(selectView({ ...general, id: "", action: "" }));
+                      setGoalData({});
+                    }}
+                    color="error"
+                  >
+                    Close
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={() => handleAdd()}
+                    color="primary"
+                  >
+                    Add
+                  </Button>
+                </Stack>
+              </DemoItem>
+            </DemoContainer>
+          </LocalizationProvider>{" "}
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddGoal;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,88 +1,363 @@
-import { Box, Button, IconButton, Stack } from '@mui/material'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { selectView } from '../redux/generalSlice';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-const Navbar = () => {
-    const general = useSelector((state) => state.general);
-    const dispatch = useDispatch();
-  return (
-    <div> <Stack
-    display={"flex"}
-    justifyContent={"space-around"}
-    direction={"row"}
-    py={2}
-  >
-    <Button
-    size='small'
-      variant={general.dayView === "daily" ? "contained" : "outlined"}
-      onClick={() =>
-        dispatch(selectView({ ...general, dayView: "daily" }))
-      }
-    >
-      Daily
-    </Button>{" "}
-    <Button
-    size='small'
-      variant={general.dayView === "weekly" ? "contained" : "outlined"}
-      onClick={() =>
-        dispatch(selectView({ ...general, dayView: "weekly" }))
-      }
-    >
-      Weekly
-    </Button>{" "}
-    <Button
-    size='small'
-      variant={general.dayView === "monthly" ? "contained" : "outlined"}
-      onClick={() =>
-        dispatch(selectView({ ...general, dayView: "monthly" }))
-      }
-    >
-      Monthly
-    </Button>{" "}
-    <Button
-    size='small'
-      variant={general.dayView === "yearly" ? "contained" : "outlined"}
-      onClick={() =>
-        dispatch(selectView({ ...general, dayView: "yearly" }))
-      }
-    >
-      Yearly
-    </Button>{" "}
-    <Button
-    size="small"
-      variant={general.dayView === "future" ? "contained" : "outlined"}
-      onClick={() =>
-        dispatch(selectView({ ...general, dayView: "future" }))
-      }
-    >
-      Future
-    </Button>{" "}
-    <Button
-    size="small"
-      variant={general.dayView === "all" ? "contained" : "outlined"}
-      onClick={() =>
-        dispatch(selectView({ ...general, dayView: "all" }))
-      }
-    >
-      All
-    </Button>{" "}
-    <Box
-sx={{
- 
- 
-  bgcolor: 'background.default',
-  color: 'text.primary',
-  borderRadius: 1,
-}}
->
-<IconButton sx={{ ml: 1 }} onClick={()=>dispatch(selectView({...general, theme: !general.theme}))} color="inherit">
-  {general.theme ? <Brightness7Icon /> : <Brightness4Icon />}
-</IconButton>
-</Box>
-  </Stack></div>
-  )
-}
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import ViewDayIcon from "@mui/icons-material/ViewDay";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { selectView } from "../redux/generalSlice";
+import HistoryIcon from "@mui/icons-material/History";
+import {
+  Divider,
+  FormControl,
+  InputAdornment,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { fetchData } from "../redux/goalSlice";
+import { getTodos, updateTodo } from "../Apis";
+import moment from "moment";
+export default function Navbar() {
+  const general = useSelector((state) => state.general);
+  const goal = useSelector((state) => state.goal);
+  const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl2, setAnchorEl2] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const [searchtxt, setSearchtxt] = React.useState("");
+  const [Notifications, setNotifications] = React.useState([]);
+  const { dayView } = useSelector((state) => state.general);
 
-export default Navbar
+  React.useEffect(() => {
+    dispatch(fetchData({ searchtxt: searchtxt }));
+  }, [searchtxt]);
+  React.useEffect(() => {
+    getNotifications();
+  }, [goal]);
+
+  const getNotifications = () => {
+    getTodos({ viewed: "NO" }).then((res) => setNotifications(res.data));
+  };
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isNotificationOpen = Boolean(anchorEl2);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleNotificationsOpen = (event) => {
+    setAnchorEl2(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+  const handlenotificationClose = () => {
+    setAnchorEl2(null);
+    handleMobileMenuClose();
+    updateNotification();
+  };
+  const updateNotification = () => {
+    Notifications.map((x) => {
+      updateTodo({ ...x, viewed: "Yes" }).then(() => {
+        getNotifications();
+      });
+    });
+  };
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+  console.log("nots", Notifications);
+  const notificationId = "primary-notification-account-menu";
+  const renderNotificationMenu = (
+    <Menu
+      anchorEl={anchorEl2}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      id={notificationId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "center",
+      }}
+      open={isNotificationOpen}
+      onClose={handlenotificationClose}
+    >
+      {Notifications.map((x) => (
+        <>
+          <MenuItem>
+            <Stack
+              display={"flex"}
+              direction={"row"}
+              width={"200px"}
+              justifyContent={"space-between"}
+            >
+              <Typography fontSize={"14px"}>{x.title}</Typography>
+              <Typography fontSize={"14px"}>
+                {moment(x.dueDate).format("DD-MM-YYYY")}
+              </Typography>
+            </Stack>
+          </MenuItem>
+          <Divider />
+        </>
+      ))}
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <Link to={"/calendar"} style={{ textDecoration: "none" }}>
+        <MenuItem>
+          <IconButton
+            sx={{ margin: "0px 4px" }}
+            size="large"
+            aria-label="show 4 new mails"
+            color="white"
+          >
+            <Badge badgeContent={4} color="error">
+              <CalendarMonthIcon />
+            </Badge>
+          </IconButton>
+          <p style={{ color: "white" }}>Calendar</p>
+        </MenuItem>
+      </Link>
+      <Link to={"/"}>
+        <MenuItem>
+          <IconButton
+            sx={{ margin: "0px 4px" }}
+            size="large"
+            aria-label="show 4 new mails"
+            color="white"
+          >
+            <Badge badgeContent={4} color="error">
+              <ViewDayIcon />
+            </Badge>
+          </IconButton>
+          <p style={{ color: "white" }}>Events</p>
+        </MenuItem>
+      </Link>
+      <MenuItem onClick={handleNotificationsOpen}>
+        <IconButton
+          sx={{ margin: "0px 4px" }}
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={Notifications.length} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          sx={{ margin: "0px 4px" }}
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            sx={{ margin: "0px 4px", mr: 2 }}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            Goal Tracker
+          </Typography>
+          <TextField
+            sx={{ marginLeft: "28px", width: "400px" }}
+            placeholder="Search..."
+            id="input-with-icon-textfield"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
+            onChange={(e) => {
+              setSearchtxt(e.target.value);
+            }}
+          />
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              sx={{ margin: "0px 4px" }}
+              size="large"
+              aria-label="show 4 new mails"
+            >
+              <Link to={"/calendar"}>
+                <Badge badgeContent={4} color="error">
+                  <CalendarMonthIcon sx={{ color: "white" }} />
+                </Badge>
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{ margin: "0px 4px" }}
+              size="large"
+              aria-label="show 17 new notifications"
+            >
+              <Link to="/">
+                <Badge badgeContent={17} color="error">
+                  <ViewDayIcon sx={{ color: "white" }} />
+                </Badge>
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{ margin: "0px 4px" }}
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+              onClick={handleNotificationsOpen}
+            >
+              <Badge badgeContent={Notifications.length} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              sx={{ margin: "0px 4px" }}
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+              onClick={handleNotificationsOpen}
+            >
+              <Link to="/timeLine">
+                <Badge badgeContent={2} color="error">
+                  <HistoryIcon sx={{ color: "white" }} />{" "}
+                </Badge>
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{ margin: "0px 4px" }}
+              size="large"
+              aria-label="account of current user"
+              color="inherit"
+            >
+              <IconButton
+                sx={{ margin: "0px 4px", ml: 1 }}
+                onClick={() =>
+                  dispatch(selectView({ ...general, theme: !general.theme }))
+                }
+                color="inherit"
+              >
+                {general.theme ? <Brightness7Icon /> : <Brightness4Icon />}
+              </IconButton>
+            </IconButton>
+            <IconButton
+              sx={{ margin: "0px 8px" }}
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              sx={{ margin: "0px 4px" }}
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+      {renderNotificationMenu}
+    </Box>
+  );
+}
